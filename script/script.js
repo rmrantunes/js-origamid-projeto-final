15f519ff01db5793d37eeb40e312884007088d50
Rafael Antunes <rafaelmessias710@gmail.com>
2020-11-16 11:53:30 -0400

    Modal is now a class

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ import TabNav from "./modules/tabNav.js";
 import Accordion from "./modules/accordion.js";
 import ScrollSuave from "./modules/scrollSuave.js";
 import initAnimacaoScroll from "./modules/animacaoScroll.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdownMenu.js";
 import initMenuMobile from "./modules/menuMobile.js";
@@ -20,8 +20,14 @@ const tabNav = new TabNav(
 );
 tabNav.init();
 
+const modal = new Modal(
+  "[data-modal='abrir']",
+  "[data-modal='fechar']",
+  "[data-modal='container']",
+);
+modal.init();
+
 initAnimacaoScroll();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();

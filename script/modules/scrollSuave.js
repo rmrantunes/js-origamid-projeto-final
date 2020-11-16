931b1a677ef66f293132cf872e6d331d69c13e7a
Rafael Antunes <rafaelmessias710@gmail.com>
2020-11-16 10:24:49 -0400

    Scroll Suave is now a class

--- script/modules/scrollSuave.js
+++ script/modules/scrollSuave.js
@@ -1,20 +1,28 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]',
-  );
-
-  function activeScroll(event) {
-    event.preventDefault();
-
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    section.scrollIntoView({
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.links = document.querySelectorAll(links);
+    this.activeScroll = this.activeScroll.bind(this);
+    this.options = options || {
       behavior: "smooth",
       block: "start",
+    };
+  }
+
+  activeScroll(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addEvent() {
+    this.links.forEach((link) => {
+      link.addEventListener("click", (event) => this.activeScroll(event));
     });
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", activeScroll);
-  });
+  init() {
+    if (this.links.length) this.addEvent();
+    return this;
+  }
 }

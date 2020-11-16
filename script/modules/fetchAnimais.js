e6d6ec6070af08d6672fcc30a06059b07672667e
Rafael Antunes <rafaelmessias710@gmail.com>
2020-11-16 18:50:50 -0400

    AnimaNumeros is now a class

--- script/modules/fetchAnimais.js
+++ script/modules/fetchAnimais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from "./animaNumeros.js";
+import AnimaNumeros from "./animaNumeros.js";
 
 export default function initFetchAnimais() {
   function createAnimal({ especie, total }) {
@@ -17,7 +17,12 @@ export default function initFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo",
+      );
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }

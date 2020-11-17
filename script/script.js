b3be2aa881c2612c3f15fd1c2bc0db380e424bca
Rafael Antunes <rafaelmessias710@gmail.com>
2020-11-17 14:47:29 -0400

    Funcionamento is now a class

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import DropdownMenu from "./modules/dropdownMenu.js";
 import MenuMobile from "./modules/menuMobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
+import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetchAnimais.js";
 import fetchBitcoin from "./modules/fetchBitcoin.js";
 
@@ -39,8 +39,10 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
 menuMobile.init();
 
+const funcionamento = new Funcionamento("[data-semana]");
+funcionamento.init();
+
 fetchAnimais("../../animais-api.json", ".numeros-grid");
 
 // initAnimaNumeros();
-initFuncionamento();
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
